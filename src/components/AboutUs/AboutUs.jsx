235cc347fa79b0f6028fafca092b2d5794a6be36
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2023-04-25 17:15:35 +0200

    up

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,8 +1,17 @@
+import './_aboutUs.scss'
+
 function AboutUs() { 
     return (
-        <>
-            AboutUs
-        </>
+        <div className="about-us section">
+            <div className="container">
+                <h2 className="title">About Us</h2>
+                <div className="flex justify-between align-center">
+                    <p className="subtitle-big">We Strive Innovate</p>
+                    <button>Become a client</button>
+                </div>
+                <p className="subtitle"><strong>Lorem ipsum</strong> dolor, sit amet consectetur adipisicing elit. Neque repellendus facere amet, ea sapiente dolores id voluptatum sunt officia corrupti quidem quibusdam quisquam molestias! Ea nobis enim iste accusantium illo.</p>
+            </div>
+        </div>
     )
 }
 
